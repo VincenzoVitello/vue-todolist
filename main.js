1b1309dde394bb96184bfae2f73081100f87dfea
VincenzoVitello <92366183+VincenzoVitello@users.noreply.github.com>
2022-01-17 16:35:28 +0100

    Progetto finito

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
 let app = new Vue ({
     el: '#app',
     data: {
+        inputValue: '',
         elementsList: [], //creo array vuoto in cui pushare elementi
         
     },
+    methods: {
+        addAndClean: function(){
+            this.elementsList.push(this.inputValue);
+            this.inputValue = "";
+        },
+        removeItem: function(){
+            this.elementsList.pop()
+        }
+    }
 })
-console.log(elementsList)
+
